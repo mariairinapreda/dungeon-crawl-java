7d370b902e2bb83bd97d3900c856b06ccc771ba0
mariairinapreda <mariairina.predaeu@gmail.com>
2022-10-01 19:02:20 +0300

    reformat code

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDao.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDao.java
@@ -6,9 +6,14 @@ import java.util.List;
 
 public interface GameStateDao {
     void add(GameState state, int playerId);
+
     void update(GameState state, int id, int playerId);
+
     GameState get(int id);
+
     GameState get(String name);
+
     List<GameState> getAll();
+
     int getPlayerId(int game_id);
 }
