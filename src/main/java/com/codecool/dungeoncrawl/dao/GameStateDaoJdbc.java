09f95cd8bfefcd310328ca480179973f5705d557
mariairinapreda <mariairina.predaeu@gmail.com>
2022-10-01 19:02:48 +0300

    reformat code

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
@@ -1,4 +1,5 @@
 package com.codecool.dungeoncrawl.dao;
+
 import com.codecool.dungeoncrawl.model.GameState;
 import com.codecool.dungeoncrawl.model.PlayerModel;
 
@@ -14,11 +15,13 @@ public class GameStateDaoJdbc implements GameStateDao {
     private DataSource dataSource;
     private PlayerDaoJdbc playerDaoJdbc;
     private PlayerDao player;
-    public  GameStateDaoJdbc(DataSource dataSource, PlayerDao player) {
+
+    public GameStateDaoJdbc(DataSource dataSource, PlayerDao player) {
         this.dataSource = dataSource;
-        this.player=player;
+        this.player = player;
     }
-    public java.sql.Date getDateNow(){
+
+    public java.sql.Date getDateNow() {
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Date date = new Date();
         return java.sql.Date.valueOf(dateFormat.format(date));
@@ -69,9 +72,9 @@ public class GameStateDaoJdbc implements GameStateDao {
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
-            if(!resultSet.next()) return null;
-            PlayerModel player=playerDaoJdbc.get(resultSet.getInt(3));
-            GameState state = new GameState(resultSet.getString(1), resultSet.getDate(2),player, resultSet.getInt(4), resultSet.getString(5));
+            if (!resultSet.next()) return null;
+            PlayerModel player = playerDaoJdbc.get(resultSet.getInt(3));
+            GameState state = new GameState(resultSet.getString(1), resultSet.getDate(2), player, resultSet.getInt(4), resultSet.getString(5));
             state.setId(id);
             return state;
         } catch (SQLException e) {
@@ -85,9 +88,9 @@ public class GameStateDaoJdbc implements GameStateDao {
             String sql = "SELECT id, current_map, saved_at, player_id ,actual_map, name FROM game_state";
             ResultSet resultSet = conn.createStatement().executeQuery(sql);
             List<GameState> result = new ArrayList<>();
-            while(resultSet.next()) {
-                PlayerModel player= new PlayerDaoJdbc(dataSource).get(resultSet.getInt(4));
-                GameState state = new GameState(resultSet.getString(2), resultSet.getDate(3),player, resultSet.getInt(5), resultSet.getString(6));
+            while (resultSet.next()) {
+                PlayerModel player = new PlayerDaoJdbc(dataSource).get(resultSet.getInt(4));
+                GameState state = new GameState(resultSet.getString(2), resultSet.getDate(3), player, resultSet.getInt(5), resultSet.getString(6));
                 state.setId(resultSet.getInt(1));
                 result.add(state);
             }
@@ -96,16 +99,18 @@ public class GameStateDaoJdbc implements GameStateDao {
             throw new RuntimeException(e);
         }
     }
+
     @Override
     public GameState get(String name) {
-        List<GameState> games=getAll();
-        for(GameState gameState: games){
-            if(gameState.getName()!=null) {
+        List<GameState> games = getAll();
+        for (GameState gameState : games) {
+            if (gameState.getName() != null) {
                 if (gameState.getName().equals(name)) return gameState;
             }
         }
         return null;
     }
+
     @Override
     public int getPlayerId(int game_id) {
         try (Connection conn = dataSource.getConnection()) {
@@ -113,7 +118,7 @@ public class GameStateDaoJdbc implements GameStateDao {
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.setInt(1, game_id);
             ResultSet resultSet = statement.executeQuery();
-            if(!resultSet.next()) return -1;
+            if (!resultSet.next()) return -1;
             return resultSet.getInt(1);
         } catch (SQLException e) {
             throw new RuntimeException(e);
